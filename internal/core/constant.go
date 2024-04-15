7a7c1c78651f6ed206cc1a8b81e4048d6ae99384
马克西Max <liuzhaomax@163.com>
2024-04-15 20:24:40 +0800

    Initial commit

---
+++ internal/core/constant.go
@@ -0,0 +1,45 @@
+package core
+
+// config dir
+const configDir = "environment/config"
+
+// logger
+const SUCCESS = "成功"
+const FAILURE = "失败"
+
+// vault
+const (
+	KV     = "kv"
+	PWD    = "pwd"
+	RSA    = "rsa"
+	JWT    = "jwt"
+	SECRET = "secret"
+	SALT   = "salt"
+	PUK    = "puk"
+	PRK    = "prk"
+	ID     = "id"
+	APP    = "app"
+)
+
+const (
+	EmptyString = ""
+	Bearer      = "Bearer "
+	UserID      = "userID"
+	// headers params
+	Method        = "Method"
+	URI           = "URI"
+	ClientIp      = "Client_ip"
+	UserAgent     = "User-Agent"
+	TraceId       = "Trace_id"
+	SpanId        = "Span_id"
+	ParentId      = "Parent_id"
+	Authorization = "Authorization"
+	AppId         = "App_id"
+	RequestId     = "Request_id"
+	UserId        = "User_id"
+	// redis
+	Signature = "signature"
+	// jaeger
+	Tracer = "tracer"
+	Parent = "parent"
+)
